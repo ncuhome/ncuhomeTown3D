6bdbdfaaabd87e8ba44644d78e02909c1c8db1b7
abandon888 <1876746719@qq.com>
2023-07-08 16:01:40 +0800

    feat: import router,complete router configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Leva } from 'leva'
+import { Canvas } from '@react-three/fiber'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        {/* <Route path='/' element={<PrivateRoute component={<StepHeader />} />}>
+        <Route index element={<PrivateRoute component={<BaseInfo />} />}></Route>
+        <Route path="/step1" element={<PrivateRoute component={<BaseQuality />} />}> </Route>
+      </Route>
+      <Route path='/login' element={<Login />}></Route>
+      <Route path='/forbidden' element={<Forbidden />}></Route> */}
+      </Routes>
     </>
   )
 }
